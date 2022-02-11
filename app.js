8c8c30816f0b7795037701bd293f8ee22bef21db
Kyle Fagnan <kfagnan19@gmail.com>
2022-02-11 15:25:25 -0700

    creating boards and generate computer ships

---
+++ app.js
@@ -0,0 +1,92 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const userGrid = document.querySelector(".grid-user");
+  const computerGrid = document.querySelector(".grid-computer");
+  const displayGrid = document.querySelector(".grid-display");
+  const ships = document.querySelectorAll(".ship");
+  const destroyer = document.querySelector(".destroyer-container");
+  const submarine = document.querySelector(".submarine-container");
+  const cruiser = document.querySelector(".cruiser-container");
+  const battleship = document.querySelector(".battleship-container");
+  const carrier = document.querySelector(".carrier-container");
+  const startButton = document.querySelector("#start");
+  const rotateButton = document.querySelector("#rotate");
+  const turnDisplay = document.querySelector("#whos-go");
+  const infoDisplay = document.querySelector("#info");
+  const userSqaures = [];
+  const computerSqaures = [];
+  const width = 10;
+
+  //create boards 
+  function createBoards(grid, sqaures) {
+    for (let i = 0; i < width * width; i++) {
+      const sqaure = document.createElement("div");
+      sqaure.dataset.id = i;
+      grid.appendChild(sqaure);
+      sqaures.push(sqaure);
+    }
+  }
+
+  createBoards(userGrid, userSqaures);
+  createBoards(computerGrid, computerSqaures);
+
+  const shipArray = [
+    {
+      name: 'destroyer',
+      directions: [
+        [0, 1],
+        [0, width]
+      ]
+    },
+    {
+      name: 'submarine',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'cruiser',
+      directions: [
+        [0, 1, 2],
+        [0, width, width*2]
+      ]
+    },
+    {
+      name: 'battleship',
+      directions: [
+        [0, 1, 2, 3],
+        [0, width, width*2, width*3]
+      ]
+    },
+    {
+      name: 'carrier',
+      directions: [
+        [0, 1, 2, 3, 4],
+        [0, width, width*2, width*3, width*4]
+      ]
+    },
+  ]
+
+  //place computer ships randomly
+  function generate(ship) {
+    let randomDirection = Math.floor(Math.random() * ship.directions.length);
+    let current = ship.directions[randomDirection];
+    if (randomDirection === 0) direction = 1;
+    if (randomDirection === 1) direction = 10;
+    let randomStart = Math.abs(Math.floor(Math.random() * computerSqaures.length - (ship.directions[0].length)));
+
+    const isTaken = current.some(index => computerSqaures[randomStart + index].classList.contains('taken'));
+    const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1);
+    const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0);
+
+    if(!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSqaures[randomStart + index].classList.add('taken', ship.name));
+
+    else generate(ship);
+  };
+
+  generate(shipArray[0]);
+  generate(shipArray[1]);
+  generate(shipArray[2]);
+  generate(shipArray[3]);
+  generate(shipArray[4]);
+});
